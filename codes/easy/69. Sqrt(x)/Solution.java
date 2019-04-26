2df1cc241a0c90edef771a90568b82d149ad2720
Lirance Zhang <zhangch3@oregonstate.edu>
2019-04-25 23:37:34 -0700

    Update Solution.java

--- codes/easy/69. Sqrt(x)/Solution.java
+++ codes/easy/69. Sqrt(x)/Solution.java
@@ -1,21 +1,17 @@
 class Solution {
-    
-    public int[] twoSum(int[] numbers, int target) {
-        int low = 0, hight = numbers.length-1;
-        
-        while(low<hight){
-            int sum = numbers[low]+numbers[hight];
-            
-            if(sum == target){
-                int []result = {low+1,hight+1};
-                return result;
-            }else if(sum < target){
-                low++;
-            }else{
-                hight--;
+    public int mySqrt(int x) {
+        if (x == 0)
+        return 0;
+        int left = 1, right = Integer.MAX_VALUE;
+        while (true) {
+            int mid = left + (right - left)/2;
+            if (mid > x/mid) {
+                right = mid - 1;
+            } else {
+                if (mid + 1 > x/(mid + 1))
+                    return mid;
+                left = mid + 1;
             }
         }
-        return new int[2];
     }
-    
 }
