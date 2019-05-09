0db7bd4f9368c88fe2b57177f71292a3d76ab188
Lirance Zhang <zhangch3@oregonstate.edu>
2019-05-09 00:39:16 -0700

    Update Solution.java

--- codes/easy/485. Max Consecutive Ones/Solution.java
+++ codes/easy/485. Max Consecutive Ones/Solution.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int temp = 0;
+        for(int n:nums){
+            if(n==1){
+                temp++;
+            }else{
+                temp = 0;
+            }
+             max = Math.max(temp,max);
+        }
+        return max;
+    }
+}
