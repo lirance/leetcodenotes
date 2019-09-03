f126af89b7c373605aa34486a0269d10af177af7
Lirance Zhang <zhangch3@oregonstate.edu>
2019-09-02 23:56:02 -0700

    Create 278. First Bad Version(E).java

---
+++ Binary Search/278. First Bad Version(E).java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n==1 && isBadVersion(0)){
+            return n;
+        }
+        
+        int left = 0, right = n-1;
+        
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            
+            if(isBadVersion(mid)){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
