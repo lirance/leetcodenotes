50331b29f6c93f9e196659f1805b95f8a610c539
Chendi Zhang <zhangch3@oregonstate.edu>
2020-01-06 00:42:05 -0800

    Create 937. Reorder Data in Log Files.java

---
+++ amazon_tag/easy/937. Reorder Data in Log Files.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        Arrays.sort(logs, (log1,log2) ->{
+            String[] split1 = log1.split(" ", 2);
+            String[] split2 = log2.split(" ", 2);
+            
+            boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+            
+            //both not digit
+            if(!isDigit1 && !isDigit2){
+                int cmp = split1[1].compareTo(split2[1]);
+                if(cmp!=0) return cmp;
+                return split1[0].compareTo(split2[0]);
+            }
+            
+            // if first is not digit, return bigger, 
+            //else if 2 is digit, equal, else smaller 
+            return isDigit1 ? (isDigit2 ? 0:1) : -1;
+            
+        });
+        
+        return logs;
+        
+    }
+}
